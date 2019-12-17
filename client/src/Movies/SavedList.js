04d4b2b0f14d27564818cc3e57a39515a82507b7
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2019-12-17 13:38:17 -0600

    Completed MVP

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
-const SavedList = props => (
-  <div className="saved-list">
+
+
+function SavedList(props){
+  const history = useHistory();
+  const homeRoute = () => {
+    setTimeout(() => {
+      history.push("/")
+    })
+  }
+
+
+  return (<div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div className="home-button" onClick={homeRoute}>Home</div>
   </div>
-);
+  );
+}
+
 
 export default SavedList;
